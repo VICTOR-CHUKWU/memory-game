54a664b3eff04b5dd3b0f0f7dfb545aeb9a947d6
victor-chukwu <okenwa1993@gmail.com>
2021-01-21 09:28:57 +0100

    worked on the restart game function

--- app.js
+++ app.js
@@ -54,31 +54,47 @@
     cardArray.sort(() => 0.5 - Math.random());
     console.log(cardArray[0].name);
 
-
+    // create a grid variable from our html grid class
     const grid = document.querySelector('.grid');
 
+
+    // to create the game board 
     function createGameBoard() {
         for(let i = 0; i < cardArray.length; i += 1){
             var card = document.createElement('img');
+
+            //setting card to a html img attribute
             card.setAttribute('src', 'images/joker1.jpg');
             card.setAttribute('data-id', i)
             card.addEventListener('click', flipCard);
+            //append our image inside the html grid class
             grid.appendChild(card);
         }
         return card;
     };
 
-    function clear() {
-        return cardArray;
-    }
     
-
+    
+  // set my card array to empty
     let cardChoosen = [];
    let cardChoosenId = [];
    let cardsWon = [];
    let resultDisplay = document.querySelector('#result');
 
+   function restartGame() {
+    //insert into grid element nothing which also clears  it
 
+    grid.innerHTML = null;
+
+    //clear result
+    resultDisplay.textContent = '';
+    cardChoosen = [];
+    cardChoosenId = [];
+    cardsWon = [];
+    createGameBoard();
+    };
+
+   // to check if a card match the selected card in name
    function checkForMatch() {
      var cards =document.querySelectorAll('img');
      const optionOneId = cardChoosenId[0];
@@ -99,6 +115,10 @@
     resultDisplay.textContent = cardsWon.length;
     if(cardsWon.length === cardArray.length/2){
         resultDisplay.innerHTML = "Congratulations you have found them all";
+        //if game ends, clear game board
+       setTimeout( restartGame, 2000);
+        //restart game
+        // createGameBoard()
         
     }
    }
